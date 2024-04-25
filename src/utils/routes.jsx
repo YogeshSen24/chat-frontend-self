62df6ad67cf5150430f56c62538c2a6fe00e0588
YogeshSen24 <yose0026@gmail.com>
2024-04-25 18:06:23 +0530

    children to childrens in route

--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -15,7 +15,7 @@ export default routes = createBrowserRouter( [
     {
         path: "/",
         element: <Layout />,
-        children: [
+        childrens: [
             {
                 path: "",
                 element: <Home />
