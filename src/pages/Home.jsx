d88b598878a6c547f3c4017ff13c79a9a28b76a6
YogeshSen24 <yose0026@gmail.com>
2024-04-25 20:45:50 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import currentUser from "../DummyData/currentUser";
+
+import currentUser from "../DummyData/currentUser.js";
 import Container  from "../components/Container.jsx";
 import UserFirstLookCard from "../components/cards/UserFirstLookCard.jsx";
 
@@ -10,11 +10,8 @@ const Home = () => {
   return (
     <Container>
       <div className="flex flex-col w-full h-full items-center ">
-      {/* <img src={currentUser?.data?.profileImage} className="object-cover h-48 aspect-[4/3] rounded-xl "></img>
-      <h2 className="text-white text-3xl font-bold text-center my-5">Welcome back ,  {currentUser?.data?.username} </h2>
-      {copyTOClipboard(currentUser?.data?._id)} */}
-      </div>
       <UserFirstLookCard name={currentUser?.data.username} email={currentUser?.data.email} bio={currentUser?.data.bio} profielImg={currentUser?.data.profileImage} mutuals={currentUser?.data.friends} />
+      </div>
     </Container>
   );
 };
